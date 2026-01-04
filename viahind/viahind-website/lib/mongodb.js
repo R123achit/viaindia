2405ec79123a55a6fe4bac54034c3b66a8c5a2ba
R123achit <rachitkesarwani570@gmail.com>
2026-01-05 05:11:13 +0530

    Fix ESLint import and MongoDB build error

--- viahind/viahind-website/lib/mongodb.js
+++ viahind/viahind-website/lib/mongodb.js
@@ -1,24 +1,30 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local');
-}
-
 const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
 let clientPromise;
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+// Only throw error if MongoDB is actually being used (not during build)
+if (!uri) {
+  if (process.env.NODE_ENV === 'production' && process.env.VERCEL) {
+    // During Vercel build, create a dummy promise that will fail at runtime if used
+    clientPromise = Promise.reject(new Error('MongoDB URI not configured'));
+  } else {
+    throw new Error('Please add your MongoDB URI to .env.local');
   }
-  clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+  }
 }
 
 export default clientPromise;
